f1adeae1ebe14871ab4b00f4fbd604dc10c4e46a
arun199529 <45041035+arun199529@users.noreply.github.com>
2020-03-06 18:44:27 -0800

    User profile updated

--- getUserProfile.php
+++ getUserProfile.php
@@ -3,20 +3,25 @@
 include 'database_connection.php';
 $uname=$_GET["uname"];
 
-$query = "SELECT * FROM tb_im_user_registration where 
+$query_json = "SELECT * FROM tb_im_user_registration where uname='$uname' limit 1;";
+$result = mysqli_query($con,$query_json);
 
-uname='$uname' limit 1;";
 
-$result = mysqli_query($con,$query);
 
-$rows = array();
 
+	    $rows = array();
 while($row = mysqli_fetch_assoc($result)) {
+$rows[] = array('id' => $row['id'],'name' => $row['name'],'phone' => $row['phone'],'restaurant_name' => $row['restaurant_name'],'emailid' => $row['emailid'],'pwd' => $row['pwd'],'pincode' => $row['postalcode'],'locality' => $row['locality'],'shipping_address' => $row['shipping_address']);
 
-$rows[] = array('id' => $row['id'],'name' => $row['name'],'phone' => $row['phone'],'emailid' => $row['emailid'],'pwd' => $row['pwd']);
+    
+}
+if(sizeof($rows)>0){
+echo json_encode($rows);
+}
+else{
+    echo 'No data found';
 }
 
-echo json_encode($rows);
-
 mysqli_close($con);
+	     
 ?>
