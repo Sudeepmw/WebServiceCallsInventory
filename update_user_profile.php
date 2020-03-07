5b61c247e8212e3d02dfe97dd0f563f478bcac3c
arun199529 <45041035+arun199529@users.noreply.github.com>
2020-03-06 18:48:42 -0800

    Update User profile modified

--- update_user_profile.php
+++ update_user_profile.php
@@ -7,14 +7,18 @@ $phone=$_GET["phone"];
 $emailid=$_GET["emailid"];
 $uname=$_GET["uname1"];
 $pwd=$_GET["pwd1"];
+$res=$_GET["res"];
+$pincode =$_GET["pincode"];
+$locality=$_GET["locality"];
+$shipping_address=$_GET["shipping_address"];
 
 
-$query="update tb_im_user_registration set name='$name',phone='$phone',emailid='$emailid',pwd='$pwd' where uname='$uname'";
 
+$query_dis="update tb_im_user_registration set name='$name',phone='$phone',emailid='$emailid',pwd='$pwd',postalcode='$pincode',locality='$locality',shipping_address='$shipping_address',restaurant_name='$res' where uname='$uname'";
 
-$result = mysqli_query($con,$query);
+$retval_dis = mysqli_query($con,$query_dis);
 
 mysqli_close($con);
 
-echo '{"message":"Updated successfully.","status":"true"}';
+echo '{"Message":"Updated successfully.","status":"true"}';
 ?>
