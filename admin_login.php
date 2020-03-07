7fd6933d81b4d01ba02b90d53e10ab0fa7603137
Sudeepmw <45040793+Sudeepmw@users.noreply.github.com>
2020-03-06 18:24:56 -0800

    Admin login Modified

--- admin_login.php
+++ admin_login.php
@@ -1,21 +1,16 @@
 <?php
-
-include 'database_connection.php';
-
 $uname=$_GET["uname"];
 $pwd=$_GET["pwd"];
 
+            include 'database_connection.php';
            
-$query = "SELECT * from tb_im_admin where uname='$uname' and pwd='$pwd';";
-            
-$result = mysqli_query($con,$query);
-
-$row = mysqli_fetch_array($result);
-
-if(!$row)
-{
-echo '{"message":"Invalid username/password","status":"false"}';
-}else{
-echo '{"message":"Login is successfully","status":"true"}';
-}
+            $query_json = "SELECT * from tb_im_admin where uname='$uname' and pwd='$pwd';";
+            $result = mysqli_query($con,$query_json);
+            $row = mysqli_fetch_array($result);
+            if(!$row)
+            {
+                echo '{"Message":"Invalid username / password","status":"false"}';
+            }else{
+                echo '{"Message":"Login successfully","status":"true"}';
+            }
 ?>
