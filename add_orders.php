f2d15180d81911feff8d907f47b22a3a31b4f67f
Sudeepmw <45040793+Sudeepmw@users.noreply.github.com>
2020-03-06 18:23:20 -0800

    add orders updated

--- add_orders.php
+++ add_orders.php
@@ -8,13 +8,24 @@ $order_date=$_GET["order_date"];
 $order_created_by=$_GET["order_created_by"];
 $quantity=$_GET["quantity"];
 $restaurant_name=$_GET["restaurant_name"];
+$updated_str=$_GET["updated_str"];
 
-$query="insert into tb_im_orders(items,price,status,order_date,order_created_by,quantity,restaurant_name) values('$items',$price,'In Process','$order_date','$order_created_by','$quantity','$restaurant_name');";
+$con=mysqli_connect($hostname, $username, $password,$dbname);
+mysqli_query ($con,"set character_set_results='utf8'");
+$query_dis="insert into tb_im_orders(items,price,status,order_date,order_created_by,quantity,restaurant_name) values('$items',$price,'In Process','$order_date','$order_created_by','$quantity','$restaurant_name');";
+
+$retval_dis = mysqli_query($con,$query_dis);
 
 
-$result= mysqli_query($con,$query);
 
-mysqli_close($con);
-
+$res=explode("|",$updated_str);
+$qstr="";
+for($i=0;$i<sizeof($res);$i++){
+    $res1=explode("@",$res[$i]);
+    $qstr="update tb_im_items set quantity=quantity-".$res1[0]." where item_name='".$res1[1]."'";
+    mysqli_query($con,$qstr);
+    
+}
 echo '{"message":"Ordered successfully.","status":"true"}';
+mysqli_close($con);
 ?>
