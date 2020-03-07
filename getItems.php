b287463b45837679f2e1db69342e54045de0a706
arun199529 <45041035+arun199529@users.noreply.github.com>
2020-03-06 18:40:03 -0800

    Get items updated

--- getItems.php
+++ getItems.php
@@ -3,15 +3,18 @@
 include 'database_connection.php';
 
 
-$query = "SELECT * FROM tb_im_items;";
-$result = mysqli_query($con,$query);
+$query_json = "SELECT * FROM tb_im_items;";
+$result = mysqli_query($con,$query_json);
 $rows = array();
-
 while($row = mysqli_fetch_assoc($result)) {
-$rows[] = array('id' => $row['id'],'item_name' => $row['item_name'],'price' => $row['price']);
+$rows[] = array('id' => $row['id'],'item_name' => $row['item_name'],'price' => $row['price'],'item_img_url' => $row['item_img_url'],'quantity' => $row['quantity']);
 }
 
+if(sizeof($rows)>0){
 echo json_encode($rows);
+}else{
+    echo 'No data found';
+}
 
 mysqli_close($con);
 ?>
