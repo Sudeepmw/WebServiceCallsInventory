d56942e26cfb04c031426cee4ab5fe7b6829a0c6
arun199529 <45041035+arun199529@users.noreply.github.com>
2020-03-06 18:42:22 -0800

    Get User orders updated

--- getUserOrders.php
+++ getUserOrders.php
@@ -3,16 +3,18 @@
 include 'database_connection.php';
 $order_created_by=$_GET["uname"];
 
-$query = "SELECT * FROM tb_im_orders where order_created_by='$order_created_by';";
-
-$result = mysqli_query($con,$query);
-
+$query_json = "SELECT * FROM tb_im_orders where order_created_by='$order_created_by' order by id desc;";
+$result = mysqli_query($con,$query_json);
 $rows = array();
 while($row = mysqli_fetch_assoc($result)) {
-$rows[] = array('quantity' => $row['quantity'],'id' => $row['id'],'items' => $row['items'],'price' => $row['price'],'status' => $row['status'],'order_date' => $row['order_date'],'order_created_by' => $row['order_created_by']);
+$rows[] = array('quantity' => $row['quantity'],'id' => $row['id'],'items' => $row['items'],'price' => $row['price'],'status' => $row['status'],'order_date' => $row['order_date'],'order_created_by' => $row['order_created_by'],'restaurant_name' => $row['restaurant_name']);
 }
 
+if(sizeof($rows)>0){
 echo json_encode($rows);
+}else{
+    echo 'No data found';
+}
 
 mysqli_close($con);
 ?>
