bda9441b1c3cc130196486d1e50c4b7a84eb4f90
Sudeepmw <45040793+Sudeepmw@users.noreply.github.com>
2020-03-06 18:22:14 -0800

    add items modified

--- add_items.php
+++ add_items.php
@@ -1,27 +1,37 @@
 <?php
 
 include 'database_connection.php';
- 
-$item_name=$_GET["item_name"];
-$price=$_GET["price"];
 
+if (!is_dir('images/')) {
+    mkdir('images/', 0777, true);
+}
 
-$query="insert into tb_im_items(item_name,price) values('$item_name','$price');";
+$quantity=$_POST["quantity"];
+$item_name=$_POST["item_name"];
+$item_name=trim($item_name,'"');
 
+$price=$_POST["price"];
+$price=trim($price,'"');
 
-
-$result = mysqli_query($con,$query);
-
-
-if($result)
+$result = array("success" => $_FILES["file"]["name"]);
+$file_path = basename( $_FILES['file']['name']);
+$img_url='images/'.$file_path;
+if(move_uploaded_file($_FILES['file']['tmp_name'], 'images/'.$file_path)) {
+    $result = array("success" => "File successfully uploaded");
+} else{
+    $result = array("success" => "error uploading file");
+}
+$con=mysqli_connect($hostname, $username, $password,$dbname);
+$query="insert into tb_im_items(item_name,price,item_img_url,quantity) values('$item_name',$price,'$img_url',$quantity);";
+$retval_dis = mysqli_query($con,$query);
+if($retval_dis)
 {
-   echo '{"message":"Item is added  successfully.","status":"true"}'; 
+   echo '{"message":"Item is added successfully.","status":"true"}'; 
 }
 else
 {
     echo '{"message":"Item not added.","status":"false"}';
 }
-
 mysqli_close($con);
 
 ?>
