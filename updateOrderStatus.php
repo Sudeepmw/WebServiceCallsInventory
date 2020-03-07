b07906d53095bcf742cbb5809f32d73ae2baa68b
arun199529 <45041035+arun199529@users.noreply.github.com>
2020-03-06 18:47:54 -0800

    Order status updated

--- updateOrderStatus.php
+++ updateOrderStatus.php
@@ -5,27 +5,44 @@ include 'database_connection.php';
 $status=$_GET["status"];
 $id=$_GET["id"];
 
+$query="update tb_im_orders set status='$status' where id=$id";
+$retval_dis = mysqli_query($con,$query);
 
-$query= "SELECT emailid FROM tb_im_user_registration WHERE uname=(SELECT order_created_by FROM tb_im_orders WHERE id=$id)";
-$result1 = mysqli_query($con,$query);
-$emailId="namaiarun@gmail.com";
-while($row = mysqli_fetch_assoc($result1)) {
+$query="SELECT emailid FROM tb_im_user_registration WHERE uname=(SELECT order_created_by FROM tb_im_orders WHERE id=$id)";
+$retval_json1 = mysqli_query($con,$query);
+
+while($row = mysqli_fetch_assoc($retval_json1)) {
 $emailId=$row['emailid'];
 }
-echo $emailId;
+
+$query="SELECT * FROM tb_im_orders WHERE id=$id";
+$retval_json2 = mysqli_query($con,$query);
+$items="";
+$price="";
+while($row = mysqli_fetch_assoc($retval_json2)) {
+$items=$row['items'];
+$price=$row['price'];
+}
+
 mysqli_close($con);
-    $to = $emailId;    
+
+    $to = $emailId;
+    
     $subject = "Your order status : ";
     $message = "";
         
          if($status=="Delivered"){
-            $message = "Your order is delivered sucessfully."; 
+            $message = "Your order with reference number: <b>".$id."</b> has been delivered sucessfully."."\r\n <b> Order Details </b>:" .$items."\r\n <b> Order Price </b> : ".$price; 
+           
          }else{
-             $message = "Sorry, your order is cancelled due to out of stock.";
+             $message = "Sorry, your order with Reference ID :<b> ".$id." </b> has been cancelled.";
          }
-         
+        
             $header = "From:namaiarun@gmail.com \r\n";
-         $result2 = mail ($to,$subject,$message,$header);
+         $header .= "MIME-Version: 1.0\r\n";
+         $header .= "Content-type: text/html\r\n";
+         $retval = mail ($to,$subject,$message,$header);
+         
 
 echo '{"message":"Updated successfully.","status":"true"}';
 ?>
