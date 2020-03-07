4a7ff0cadd6ad12c6cf2c63ce45c481c2142cce4
Sudeepmw <45040793+Sudeepmw@users.noreply.github.com>
2020-03-06 18:28:10 -0800

    Update edit_item.php

--- edit_item.php
+++ edit_item.php
@@ -5,13 +5,14 @@ include 'database_connection.php';
 $item_name=$_GET["item_name"];
 $price=$_GET["price"];
 $id=$_GET["id"];
+$quantity=$_GET["quantity"];
 
-
-$query="update tb_im_items set item_name='$item_name',price='$price' where id='$id'";
+$query="update tb_im_items set item_name='$item_name',price=$price,quantity=$quantity where id='$id'";
 
 $result = mysqli_query($con,$query);
+echo $result;
+echo '{"message":"Data is updated successfully.","status":"OK"}';
 
 mysqli_close($con);
 
-echo '{"message":"Data is updated successfully.","status":"true"}';
 ?>
